b798e2ca0dd3be59a22f3c3f92c9414ee79ad9ed
Dudekulasameera19 <dsameeraiiit@gmail.com>
2026-01-10 18:05:10 +0530

    Variables

---
+++ Variables/app.js
@@ -0,0 +1,23 @@
+// name age is student
+var hi = "sameera"
+console.log(typeof(hi))
+var age = 30
+console.log(typeof(age))
+var boolean = "isvalid"
+console.log(typeof(boolean))
+
+console.log("#########")
+let hello = "sameera"
+console.log(typeof(hello))
+let age1 = 19
+console.log(typeof(age1))
+let check = "isvalid"
+console.log(typeof(check))
+
+console.log("@@@@@@@@@@@@")
+const hello1 = "sameera"
+console.log(typeof(hello1))
+let age2 = 90
+console.log(typeof(age2))
+let check1 = "isvalid"
+console.log(typeof("check1"))
